a73f6ded948c42dfa3d7cd5d3bafe16b647978a3
Priyanshu-FirsThing <167760124+Priyanshu-FirsThing@users.noreply.github.com>
2024-06-10 17:51:29 +0530

    reverted all the changes

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -22,7 +22,7 @@ const Hero = () => {
         setCurrentMessageIndex((prevIndex) => (prevIndex + 1) % messages.length);
         setFade(false);
       }, 500); // Wait for the fade-out animation duration before changing the message
-    }, 4000); // Change text every 4 seconds (2s fade-in + 2s fade-out)
+    }, 2000); // Change text every 4 seconds (2s fade-in + 2s fade-out)
 
     return () => clearInterval(interval); // Clear interval on component unmount
   }, [messages.length]);
@@ -31,44 +31,22 @@ const Hero = () => {
     <>
       <section
         id="home"
-<<<<<<< HEAD
-        className="relative z-10 overflow-hidden bg-white dark:bg-gray-dark  sm:pt-7 xl:pb-[160px] xl:pt-[180px] 2xl:pb-[200px] 2xl:pt-[210px]"
-=======
-        className="relative z-10 sm:h-screen h-screen overflow-hidden bg-white dark:bg-gray-dark md:pb-[120px] md:pt-[150px] sm:pt-7 xl:pb-[160px] xl:pt-[180px] 2xl:pb-[200px] 2xl:pt-[210px]"
->>>>>>> origin/small-fixes
+        className="relative z-10 overflow-hidden bg-white dark:bg-gray-dark md:pb-[120px] md:pt-[150px] sm:pt-7 xl:pb-[160px] xl:pt-[180px] 2xl:pb-[200px] 2xl:pt-[210px]"
       >
         <div className="container h-full">
           <div className="-mx-4 flex flex-wrap h-full">
             <div className="w-full px-5 h-full">
               {/* Video for desktop */}
               <video
-<<<<<<< HEAD
                 className="absolute left-0 top-0 w-full h-full object-cover opacity-30"
-=======
-                className="absolute left-0 top-0 w-full h-full object-cover hidden sm:block opacity-30"
->>>>>>> origin/small-fixes
                 src={'/images/video/videoplayback.mp4'}
                 autoPlay
                 loop
                 muted
                 playsInline
               />
-<<<<<<< HEAD
               <div className="relative pt-20 pb-12 sm:pt-0 mx-auto max-w-[100%] sm:max-w-[800px] text-center h-full flex flex-col justify-center sm:justify-center">
-                <h1 className={`text-2xl font-semibold leading-tight text-white dark:text-white md:text-3xl md:leading-tight transition-opacity ${fade ? 'opacity-0' : 'opacity-100'}`}>
-=======
-              {/* Video for mobile */}
-              <video
-                className="absolute left-0 top-0 w-full h-full object-cover sm:hidden opacity-30"
-                src={'/images/video/mobilevideoplayback.mp4'}
-                autoPlay
-                loop
-                muted
-                playsInline
-              />
-              <div className="relative pt-20 pb-12 sm:pt-0 mx-auto max-w-[100%] sm:max-w-[800px] text-center h-full flex flex-col justify-center sm:justify-center">
-                <h1 className={`translate-y-28 xl:translate-y-10 text-2xl font-semibold leading-tight text-white dark:text-white md:text-3xl md:leading-tight transition-opacity ${fade ? 'opacity-0' : 'opacity-100'}`}>
->>>>>>> origin/small-fixes
+                <h1 className={`text-xl font-semibold leading-tight text-white dark:text-white md:text-3xl md:leading-tight transition-opacity ${fade ? 'opacity-0' : 'opacity-100'}`}>
                   {messages[currentMessageIndex]}
                 </h1>
                 <LottieImage className={"m-auto heroBannerBlink w-12 h-12 sm:w-36 sm:h-36"} src={animationData} />
