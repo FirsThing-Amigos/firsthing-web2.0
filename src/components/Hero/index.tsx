e5f8997317c77e4f8b663b5bdc55543664624053
Priyanshu-FirsThing <167760124+Priyanshu-FirsThing@users.noreply.github.com>
2024-07-30 04:35:41 +0530

    little changes on the home menu video

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -33,23 +33,23 @@ const Hero = () => {
         id="home"
         className="relative z-10 overflow-hidden bg-white dark:bg-gray-dark md:pb-[120px] md:pt-[150px] sm:pt-7 xl:pb-[160px] xl:pt-[180px] 2xl:pb-[200px] 2xl:pt-[210px]"
       >
-        <div className="container h-full">
-          <div className="-mx-4 flex flex-wrap h-full">
-            <div className="w-full px-5 h-full">
+        <div className="container h-[30em]">
+          <div className="-mx-4 flex flex-wrap h-full ">
+            <div className="w-full px-5 h-full ">
               {/* Video for desktop */}
               <video
-                className="absolute left-0 top-0 w-full h-full object-cover opacity-30"
+                className=" absolute left-0 top-0 w-full h-full object-cover opacity-30"
                 src={'/images/video/videoplayback.mp4'}
                 autoPlay
                 loop
                 muted
                 playsInline
               />
-              <div className="relative pt-20 pb-12 sm:pt-0 mx-auto max-w-[100%] sm:max-w-[800px] text-center h-full flex flex-col justify-center sm:justify-center">
+              <div className="relative pt-[10em] pb-10 sm:pt-0 text-center h-full flex flex-col justify-center sm:justify-center">
                 <h1 className={`text-xl font-semibold leading-tight text-white dark:text-white md:text-3xl md:leading-tight transition-opacity ${fade ? 'opacity-0' : 'opacity-100'}`}>
                   {messages[currentMessageIndex]}
                 </h1>
-                <LottieImage className={"m-auto heroBannerBlink w-12 h-12 sm:w-36 sm:h-36"} src={animationData} />
+                <LottieImage className={"m-auto heroBannerBlink h-20 w-20 z-0"} src={animationData} />
               </div>
             </div>
           </div>
